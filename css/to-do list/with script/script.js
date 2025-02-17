0419d661502c8f055784d5e6eeeeac9cc93858a1
Edwin Sal <ki2yzn@gmail.com>
2025-02-17 15:51:11 +0800

    Redo to-do-list with script.

--- css/to-do list/with script/script.js
+++ css/to-do list/with script/script.js
@@ -1,20 +1,49 @@
-const taskContainer = document.getElementById('tasks-container');
-const addTaskButton = document.getElementById('add-task');
 const taskInput = document.getElementById('task-name-input');
-
+const taskContainer = document.getElementById('tasks-container');
 const taskData = [];
 
 const addTask = () => {
-	// Get value of the input
-	taskData.push(taskInput.value);
+	const taskValue = taskInput.value;
+	if(taskValue.length === 0) return;
 
-	// Update the tasks array
-	let taskHTML = '';
-	taskData.forEach(task => taskHTML += `<p class="task">${task}</p>`);
-
-	// Display tasks from the array
-	taskContainer.innerHTML = taskHTML;
-
-	// Clear task input
+	taskData.push(taskValue);
 	taskInput.value = '';
+
+	showTask();
 };
+
+const handleSubmit = (event) => {
+	event.preventDefault();
+};
+
+const toggleTask = (id) => {
+	console.log(id);
+
+	const task = document.getElementById(`task${id}`);
+
+	if(task.classList.contains('complete')) {
+		task.classList.remove('complete')
+	} else {
+		task.classList.add('complete')
+	}
+};
+
+const showTask = () => {
+	// Save tasks as HTML elements
+	let taskHTML = '';
+	taskData.forEach((task, index) => {
+		taskHTML += `<li><button id="task${index}" onclick="toggleTask(${index})" class="task">${task}</button></li>`
+	});
+
+	console.log(taskHTML);
+
+	// Display tasks
+	taskContainer.innerHTML = taskHTML;
+};
+
+// Add event listener for add task
+document.getElementById('add-task').addEventListener('click', addTask);
+
+// Add event listener for toggling task
+
+showTask();
