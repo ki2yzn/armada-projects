996ed4e40a98883128b89d06fff3db957f3f6d05
Edwin Sal <ki2yzn@gmail.com>
2025-03-02 20:37:08 +0800

    Fix callback challenge before submission.

--- javascript/callback challenge/script.js
+++ javascript/callback challenge/script.js
@@ -1,87 +1,105 @@
 // Task 1 Custom Map Function
-function customMap(numbers, callback) {
-	const updatedArr = [];
+function task1() {
+	function customMap(numbers, callback) {
+		const updatedArr = [];
 
-	for(let number of numbers) {
-		updatedArr.push(callback(number));
+		for(let number of numbers) {
+			updatedArr.push(callback(number));
+		}
+
+		return updatedArr;
 	}
 
-	return updatedArr;
+	let numbers = [1, 2, 3];
+	let doubled = customMap(numbers, function(num) { return num * 2; });
+	console.log(doubled); // Should output: [2,4,6]
 }
-
-let numbers = [1, 2, 3];
-let doubled = customMap(numbers, function(num) { return num * 2; });
-console.log(doubled); // Should output: [2,4,6]
+// task1();
 
 
 
 // Task 2 Filter Function
-function filter(arr, callback) {
-	const updatedArr = [];
+function task2() {
+	function filter(arr, callback) {
+		const updatedArr = [];
 
-	for(let number of arr) {
-		if(callback(number)) updatedArr.push(number);
+		for(let number of arr) {
+			if(callback(number)) updatedArr.push(number);
+		}
+
+		return updatedArr;
 	}
 
-	return updatedArr;
-}
-
-let result = filter([1,2,3,4,15], function(val) { return val < 10; });
-console.log(result); // Should output: [1,2,3,4]
-
+	let result = filter([1,2,3,4,15], function(val) { return val < 10; });
+	console.log(result); // Should output: [1,2,3,4]
+};
+// task2();
 
 
-// Task 3 
-function some(arr, callback) {
-	for(let number of arr) {
-		if(callback(number)) return true;
+
+// Task 3 Some Function
+function task3() {
+	function some(arr, callback) {
+		for(let number of arr) {
+			if(callback(number)) return true;
+		}
+
+		return false;
 	}
 
-	return false;
-}
-
-let result = some([1,2,3,4], function(val) { return val>5; });
-console.log(result); 
+	let result = some([1,2,3,4], function(val) { return val>5; });
+	console.log(result); 
+};
+// task3();
 
 
 
 // Task 4 Every Function
-function every(arr, callback) {
-	for(let number of arr) {
-		if(!callback(number)) return false;
+function task4() {
+	function every(arr, callback) {
+		for(let number of arr) {
+			if(!callback(number)) return false;
+		}
+
+		return true;
 	}
 
-	return true;
-}
-
-let result = every([1,2,3,0], function(val) { return val>0; });
-console.log(result); // Should output: true
+	let result = every([1,2,3], function(val) { return val>0; });
+	console.log(result); // Should output: true
+};
+// task4();
 
 
 
 // Task 5 Reduce Function
-function reduce(arr, callback) {
-	let sum = 0;
-	for(let number of arr) {
-		sum = callback(sum, number);
+function task5() {
+	function reduce(arr, callback) {
+		let sum = 0;
+		for(let number of arr) {
+			sum = callback(sum, number);
+		}
+
+		return sum;
 	}
 
-	return sum;
-}
-
-let sum = reduce([1,2,3], function(acc, num) { return acc + num; });
-console.log(sum); // Should output: 6
+	let sum = reduce([1,2,3], function(acc, num) { return acc + num; });
+	console.log(sum); // Should output: 6
+};
+// task5();
 
 
 
 // Task 6 Includes Check
-function includes(arr, callback) {
-	for(let number of arr) {
-		if(callback(number)) return true;
+function task6() {
+	function includes(arr, callback) {
+		for(let number of arr) {
+			if(callback(number)) return true;
+		}
+
+		return false;
 	}
 
-	return false;
-}
-
-let result = includes([1,2,3], function(val) { return val===2; });
-console.log(result); // Should output: true
+	let result = includes([1,2,3], function(val) { return val===2; });
+	console.log(result); // Should output: true
+};
+// task6();
