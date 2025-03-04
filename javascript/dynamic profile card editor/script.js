801180a2630cde129300f40528d2c2530b8c1014
Edwin Sal <ki2yzn@gmail.com>
2025-03-04 17:56:25 +0800

    Finish dynamic profile card editor.

---
+++ javascript/dynamic profile card editor/script.js
@@ -0,0 +1,38 @@
+// Disable default behavior of form refreshing after clicking button
+document.getElementById('update-form').addEventListener('submit', (event) => {
+	event.preventDefault();
+});
+
+// Can update both name and bio only
+function updateInfo(inputID, elementID, type) {
+	// Get the value of new info
+	const newInfo = document.getElementById(inputID).value;
+
+	// Set the value of new info
+	document.getElementById(elementID).innerHTML = newInfo;
+
+	// Provide feedback
+	console.log(`${type} updated succesfully!`);
+};
+
+function updateImage() {
+	// Get the value of new src
+	const newSrc = document.getElementById('update-image-url').value;
+
+	// Set the new src
+	document.getElementById('user-profile-picture').setAttribute('src', newSrc);
+
+	// Provide feedback
+	console.log('Profile picture updated succesfully!');
+}
+
+function updateBackgroundColor() {
+	// Get the value of the new bg color
+	const newColor = document.getElementById('update-background-color').value;
+
+	// Set the new bg color
+	document.getElementById('profile-card').style.backgroundColor = newColor;
+
+	// Provide feedback
+	console.log('Background color updated succesfully!');
+}
