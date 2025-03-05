425a2a047ffd90df14b246c4490b4c32a3ffdc43
Edwin Sal <ki2yzn@gmail.com>
2025-03-05 21:07:34 +0800

    Redo styling for revisit bank account manager.

---
+++ javascript/revisit bank account manager/script.js
@@ -0,0 +1,60 @@
+// Disable default form submit behavior
+document.getElementById('bank-form').addEventListener('submit', (event) => {
+	event.preventDefault();
+});
+
+let balance = 0;
+const inputElement = document.getElementById('cash-input');
+
+// Display notification by modifying the notification element
+function showNotification(content, result) {
+	// Update the innertext of the notification
+	const notificationElement = document.getElementById('notification')
+	notificationElement.innerText = content;
+
+	// Change notification color based on result
+	notificationElement.style.color = result === 'success' ? 'green' : 'red'; 
+};
+
+// Deposit value by incrementing the balance 
+function doTransaction(mode) {
+	// Get the value of the cash input
+	const value = Number(inputElement.value);
+	
+	switch(mode) {
+		case 'deposit':
+			balance += value;
+			break;
+		case 'withdraw':
+			if(value <= balance && balance > 0) {
+				balance -= value;
+			} else {
+				// If balance is insufficient, show notification and stop the code here
+				showNotification('Insufficient balance!', 'fail');
+				inputElement.value = ''; //Clear the cash input
+				return;
+			}
+			break;
+		default:
+			console.log('Invalid mode!');
+	};
+
+	// Updates the balance on the page
+	document.getElementById('balance').innerText = `$${balance}`;
+
+	// Display transaciton notification
+	showNotification(`Succesfully ${mode === 'deposit' ? 'deposited' : 'withdrew'} $${value}.`, 'success');
+
+	// Clear the cash input
+	inputElement.value = '';
+};
+
+// Add event listener for the deposit button
+document.getElementById('deposit').addEventListener('click', () => {
+	doTransaction('deposit');
+});
+
+// Add event listener for the withdraw button
+document.getElementById('withdraw').addEventListener('click', () => {
+	doTransaction('withdraw');
+});
