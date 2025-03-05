e08f0f480f01acd39d8998dfbc7737c509962a5e
Edwin Sal <ki2yzn@gmail.com>
2025-03-05 15:51:23 +0800

    Add notification for extra miles.

--- javascript/working to-do list/script.js
+++ javascript/working to-do list/script.js
@@ -1,19 +1,54 @@
 // Used for adding id to task elements
 let count = 1; 
 
+// Used for resetting existing setTimeout
+let timeoutId;
+
 // Disable default behavior of form. Refreshing when submitted.
 document.getElementById('task-form').addEventListener('submit', (event) =>  {
 	event.preventDefault();
 });
 
+// Hide the notification by changing its display property to none
+function hideNotification(notification) {
+	notification.style.display = 'none';
+};
+
+// Show notification by changing the display property of the notification element
+function showNotification(content) {
+	// Get the notification element
+	const notificationElement = document.getElementById('notification');
+
+	// Set the content
+	notificationElement.innerHTML = `${content} <button onclick="hideNotification(${notificationElement.id})">X</button>`;
+
+	// Display the notification
+	notificationElement.style.display = 'flex';
+
+	// Clear previous existing timeouts
+	clearTimeout(timeoutId);
+
+	// Automatically hide the notification after 5 secs
+	timeoutId = setTimeout(() => {
+		notificationElement.style.display = 'none';
+	}, 5000);
+};
+
+// Remove the task from the dom by using remove()
 function removeTask(task, value) {
-	alert(`Task removed: ${value}`);
+	showNotification(`Task removed: ${value}`);
 	task.remove();
 }
 
 function addTask() {
 	// Get the task input
 	const taskInput = document.querySelector('.task-name-input');
+
+	// Stop the code here if input is empty
+	if(!taskInput.value) {
+		showNotification('Task is empty!');
+		return;
+	};
 
 	// Get the task container
 	const taskContainer = document.querySelector('.tasks-container');
@@ -29,7 +64,7 @@ function addTask() {
 	taskContainer.appendChild(taskElement);
 
 	// Show notification
-	alert(`Task added: ${taskInput.value}`);
+	showNotification(`Task added: ${taskInput.value}`);
 
 	// Clear the add task input
 	taskInput.value = '';
